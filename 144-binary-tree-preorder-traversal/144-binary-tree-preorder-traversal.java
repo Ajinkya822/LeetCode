b2544841ab382c5e9eb34b760c50c2ccdfcdcad6
Ajinkya <ajinkyanagare8@gmail.com>
2022-01-18 18:04:21 -0800

    Time: 1 ms (26.12%), Space: 38.9 MB (28.15%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -15,29 +15,29 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-//         List<Integer> op=new ArrayList<Integer>();
-//         Stack<TreeNode> stk=new Stack<TreeNode>();
-//         TreeNode current=root;
+        List<Integer> op=new ArrayList<Integer>();
+        Stack<TreeNode> stk=new Stack<TreeNode>();
+        TreeNode current=root;
         
-//         while(current!=null || !stk.empty()){
-//             while(current!=null){
-//                 stk.push(current);
-//                 op.add(current.val);
-//                 current=current.left;
-//             }
-//             current=stk.pop();
-//             current=current.right;
-//         }
-//         return op;
-        
-        List<Integer> op=new ArrayList<>();
-            if(root==null){
-                return op;
+        while(current!=null || !stk.empty()){
+            while(current!=null){
+                stk.push(current);
+                op.add(current.val);
+                current=current.left;
             }
-        op.add(root.val);
-        op.addAll(preorderTraversal(root.left));
-        op.addAll(preorderTraversal(root.right));
+            current=stk.pop();
+            current=current.right;
+        }
         return op;
+        
+        // List<Integer> op=new ArrayList<>();
+        //     if(root==null){
+        //         return op;
+        //     }
+        // op.add(root.val);
+        // op.addAll(preorderTraversal(root.left));
+        // op.addAll(preorderTraversal(root.right));
+        // return op;
     
     }
 }
