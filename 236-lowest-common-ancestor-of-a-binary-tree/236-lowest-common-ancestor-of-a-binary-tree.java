03748c3f8ceefcdcb0dd247fc98590b4bb7433d9
Ajinkya <ajinkyanagare8@gmail.com>
2022-01-20 22:18:59 -0800

    Time: 13 ms (25.74%), Space: 45.1 MB (40.95%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,73 +8,19 @@
  * }
  */
 class Solution {
-    int target=7;
-    
-    List<TreeNode> op1=new LinkedList<>();
-    List<TreeNode> op2=new LinkedList<>();
-    boolean isFoundp=false;
-    boolean isFoundq=false;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        //find path using dfs
-            findpathp(root,p);
-            findpathq(root,q);
-            int i=0;
-            while(true){
-                if(i<op1.size() && i<op2.size() && op1.get(i).val==op2.get(i).val){
-                    i++;
-                }
-                else{
-                    return op1.get(i-1);
-                }
-            }
-            
-    }
-    
-    public List<TreeNode> findpathp(TreeNode root, TreeNode p){
-            if(root==null)
-                return null;
-            if(p.val==root.val){
-                isFoundp=true;
-                op1.add(root);
-                return op1;
-            }
-            op1.add(root);
-            
-            if(!isFoundp){
-                findpathp(root.left,p); 
-            }
-                
-            if(!isFoundp){
-                findpathp(root.right,p);
-            }
-            if(!isFoundp){
-                op1.remove(op1.size()-1);
-            }
-
-            return op1;
-    }
-    
-    public List<TreeNode> findpathq(TreeNode root, TreeNode q){
-            if(root==null)
-                return null;
-            if(q.val==root.val){
-                isFoundq=true;
-                op2.add(root);
-                return op2;
-            }
-            op2.add(root);
-            
-            if(!isFoundq){
-               findpathq(root.left,q); 
-            }
-                
-            if(!isFoundq){
-                findpathq(root.right,q);
-            }
-            if(!isFoundq){
-                op2.remove(op2.size()-1);
-            }
-
-            return op2;
+        if(root==p || root==q || root==null){
+            return root;
+        }
+        
+        TreeNode left=lowestCommonAncestor(root.left, p, q);
+        TreeNode right=lowestCommonAncestor(root.right, p, q);
+        
+        if(left!=null && right!=null){
+            return root;
+        }
+        return left==null?right:left;
+        
     }
 }
