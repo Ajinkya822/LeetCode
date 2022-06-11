9e994286507cd766bdec6e29e9d08d12218a76d0
Ajinkya <ajinkyanagare8@gmail.com>
2022-06-11 16:20:19 -0700

    Time: 0 ms (100.00%), Space: 43.1 MB (6.24%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -1,32 +1,47 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int valPtr=0;
+        //completed without help
+        //this solution is where order matters
+        // int valPtr=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==val){
+        //         valPtr=i;
+        //         break;
+        //     }
+        // }
+        // int temp=0;
+        // for(int ptr=0;ptr<nums.length;ptr++){
+        //     if(ptr>valPtr && nums[ptr]!=nums[valPtr]){
+        //         temp=nums[ptr];
+        //         nums[ptr]=nums[valPtr];
+        //         nums[valPtr]=temp;
+        //         valPtr++;
+        //     }
+        // }
+        // int ans=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=val){
+        //         ans++;
+        //     }
+        //     else{
+        //         break;
+        //     }
+        // }
+        // return ans;
         
-        for(int i=0;i<nums.length;i++){
+        
+    //if order doesn't matter
+        int i=0;
+        int n=nums.length;
+        while(i<n){
             if(nums[i]==val){
-                valPtr=i;
-                break;
-            }
-        }
-        int temp=0;
-        for(int ptr=0;ptr<nums.length;ptr++){
-            if(ptr>valPtr && nums[ptr]!=nums[valPtr]){
-                temp=nums[ptr];
-                nums[ptr]=nums[valPtr];
-                nums[valPtr]=temp;
-                //valPtr=ptr;
-                valPtr++;
-            }
-        }
-        int ans=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val){
-                ans++;
+                nums[i]=nums[n-1];
+                n--;
             }
             else{
-                break;
+                i++;
             }
         }
-        return ans;
+        return n;
     }
 }
